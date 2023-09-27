16cd114ad490f82818a6ddc64291d4bc01ed59de
Ishwarya <137306658+Ishulucky@users.noreply.github.com>
2023-09-27 22:55:06 +0530

    Add files via upload

--- Insurance/src/main/java/com/insurance/Insurance/Controller/InsuranceRestController.java
+++ Insurance/src/main/java/com/insurance/Insurance/Controller/InsuranceRestController.java
@@ -5,42 +5,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insurance.Insurance.Repository.InsuranceRepository;
-import com.insurance.Insurance.contracts.InsuranceDAO;
-import com.insurance.Insurance.model.InsurancePackage;
 import com.insurance.Insurance.model.InsurancePolicy;
 import com.insurance.Insurance.model.InsurancePolicySchedule;
-
-
 
 @RestController
 
 public class InsuranceRestController {
 
+	@Autowired
+	private InsuranceRepository insuranceRepository;
 
-    @Autowired
-    private InsuranceRepository insuranceRepository;
-
-    @GetMapping("/policy")
+	@GetMapping("/policy")
 	public List<InsurancePolicy> getAllPolicy() {
 		return insuranceRepository.ListAllPolicy();
 	}
-    
-    @GetMapping("/policySchedule")
+
+	@GetMapping("/policySchedule")
 	public List<InsurancePolicySchedule> getAllPolicySchedule() {
 		return insuranceRepository.ListAllPolicySchedules();
+	}
+
+	@GetMapping("/policySchedule/{id}")
+	public List<InsurancePolicySchedule> listAllPolicySchedules(@PathVariable Integer id) {
+		return insuranceRepository.ListAllPolicySchedulesById(id);
 	}
 
 	@PostMapping(value = "/createpolicy")
@@ -48,24 +43,15 @@ public class InsuranceRestController {
 		insuranceRepository.createNewPolicy(u);
 		return new ResponseEntity<>("Employee is created successfully", HttpStatus.CREATED);
 	}
-	
-	@PutMapping("/updatepolicy")
-    public ResponseEntity<Object> updatePolicy(@RequestBody InsurancePolicy p) {
-        int result = insuranceRepository.createNewPolicy(p);
-        if (result > 0) {
-            return new ResponseEntity<>("Package is updated successfully", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Package update failed", HttpStatus.NOT_FOUND);
-        }
-    }
-    
-    
-    @GetMapping("/getpolicyid")
-	public String getCustomerDetails(@RequestParam("Id") String Id, Model model) {
-		model.addAttribute("customer", insuranceRepository.getPolicyeById(Integer.parseInt(Id)));
-		return "customerDetails";
-	}
-    
 
+	@PutMapping("/updatepolicy")
+	public ResponseEntity<Object> updatePolicy(@RequestBody InsurancePolicy p) {
+		int result = insuranceRepository.createNewPolicy(p);
+		if (result > 0) {
+			return new ResponseEntity<>("Package is updated successfully", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Package update failed", HttpStatus.NOT_FOUND);
+		}
+	}
 
 }
